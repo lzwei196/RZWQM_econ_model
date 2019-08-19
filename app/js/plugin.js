cffd97c0d4e7f3fd65f25341c7eb64371932946c
Leo <lzweijinwei@yahoo.ca>
2019-08-19 08:42:11 -0400

    scraper

--- app/js/plugin.js
+++ app/js/plugin.js
@@ -12,42 +12,47 @@
     })
 }
 
-function readData(){
-    // let filepath = chooseFilePath();
-    // let the user choose the analysis file and read the data from it.
-     let resultFilepath = chooseFilePath("Please choose the analysis file.");
-
-     //let data = fs.readFileSync(filepath).toString().split("\n");
-     let resultData = fs.readFileSync(resultFilepath).toString().split("\n");
-     //data.splice(974,2,"0  0  0  0.0  0.0  20.0  0.0  0")
-     //console.log(data);
-     resultData.splice(0,23);
-     //writeToLocal(data);
-     //should take in argument
-     let N2O = [];
-     let NxO = [];
-     let CO2 = [];
-     let yearDay = [];
-     for(let i = 0; i < resultData.length; i++ ){
-         let oneData = resultData[i];
-         let numbers;
-         let resultArray = []
-         while (numbers = regex.exec(oneData)) {
-             resultArray.push(numbers[0]);
-         }
-         N2O.push(resultArray[102]);
-         NxO.push(resultArray[103]);
-         CO2.push(resultArray[77]);
-         yearDay.push(resultArray[0]);
-     } 
-     console.log(N2O);
-     console.log(NxO);
-};
-
 function chooseFilePath(title){
     fileFinalName = dialog.showOpenDialog({'title':title});
     return fileFinalName[0];
   };
   
-exports.chooseFilePath = chooseFilePath();
-exports.readData = readData();
+module.exports = {
+    chooseFilePath:function chooseFilePath(title){
+        fileFinalName = dialog.showOpenDialog({'title':title});
+        return fileFinalName[0];
+      },
+    readData:function readData(){
+        // let filepath = chooseFilePath();
+        // let the user choose the analysis file and read the data from it.
+         let resultFilepath = chooseFilePath("Please choose the analysis file.");
+    
+         //let data = fs.readFileSync(filepath).toString().split("\n");
+         let resultData = fs.readFileSync(resultFilepath).toString().split("\n");
+         //data.splice(974,2,"0  0  0  0.0  0.0  20.0  0.0  0")
+         //console.log(data);
+         resultData.splice(0,23);
+         //writeToLocal(data);
+         //should take in argument
+         let N2O = [];
+         let NxO = [];
+         let CO2 = [];
+         let yearDay = [];
+         for(let i = 0; i < resultData.length; i++ ){
+             let oneData = resultData[i];
+             let numbers;
+             let resultArray = []
+             while (numbers = regex.exec(oneData)) {
+                 resultArray.push(numbers[0]);
+             }
+             N2O.push(Number(resultArray[102]));
+             NxO.push(Number(resultArray[103]));
+             CO2.push(Number(resultArray[77]));
+             yearDay.push(resultArray[0]);
+         } 
+         console.log('N20' + N2O);
+         console.log('Nx0' + NxO);
+         console.log('CO2' + CO2);
+         console.log('Date' + yearDay);
+    }
+}
---
+++ app/js/scraper.js
@@ -0,0 +1,28 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const url = {
+             'Europe from Markets insider':'https://markets.businessinsider.com/commodities/co2-european-emission-allowances'
+}
+
+const options = {
+    uri:url["Europe from Markets insider"],
+    headers:{
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    }
+}
+
+module.exports = {
+    priceScraper:function priceScraper(){
+    rp(options)
+    .then(function (html) {
+        // REQUEST SUCCEEDED: DO SOMETHING
+        const $ = cheerio.load(html);
+        const price = $('.push-data');
+        return price; 
+    })
+    .catch(function (err) {
+        // REQUEST FAILED: ERROR OF SOME KIND
+        console.log(err);
+    });
+}
+}
