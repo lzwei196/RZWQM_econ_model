828a94fe10649dc218e578d177d181470481e228
Leo <lzweijinwei@yahoo.ca>
2020-01-06 16:31:09 -0500

    update on the exchange of the unit of the crops

--- app/js/sessionStorage.js
+++ app/js/sessionStorage.js
@@ -1,26 +1,29 @@
 module.exports = {
-    location : function location(locationName){
-        sessionStorage.setItem('location',locationName)
+    location: function location(locationName) {
+        sessionStorage.setItem('location', locationName)
     },
-    project : function project(projectName){
-        sessionStorage.setItem('project',projectName)
+    project: function project(projectName) {
+        sessionStorage.setItem('project', projectName)
     },
-    mitigationMethod: function mitigationMethod(mitigationMethod){
-        sessionStorage.setItem('mitigation',mitigationMethod)
+    mitigationMethod: function mitigationMethod(mitigationMethod) {
+        sessionStorage.setItem('mitigation', mitigationMethod)
     },
-    methodDetail : function methodDetail(methodDetail){
-        sessionStorage.setItem('methodDetail',JSON.stringify(methodDetail))
+    methodDetail: function methodDetail(methodDetail) {
+        sessionStorage.setItem('methodDetail', JSON.stringify(methodDetail))
     },
-    carbonTradePrice : function carbonTradePrice(carbonTradePrice){
-        sessionStorage.setItem('carbonTradePrice',JSON.stringify(carbonTradePrice))
+    carbonTradePrice: function carbonTradePrice(carbonTradePrice) {
+        sessionStorage.setItem('carbonTradePrice', JSON.stringify(carbonTradePrice))
     },
-    projectDirectory : function projectDirectory(projectDirectory){
-        sessionStorage.setItem('projectDirectory',JSON.stringify(projectDirectory))
+    projectDirectory: function projectDirectory(projectDirectory) {
+        sessionStorage.setItem('projectDirectory', JSON.stringify(projectDirectory))
     },
-    cropsWithPrice : function cropsWithPrice(cropsWithPrice){
-        sessionStorage.setItem('cropWithPrice',JSON.stringify(cropsWithPrice))
+    cropsWithPrice: function cropsWithPrice(cropsWithPrice) {
+        sessionStorage.setItem('cropWithPrice', JSON.stringify(cropsWithPrice))
     },
-    plant : function plant(plant){
-        sessionStorage.setItem('plant_date',JSON.stringify(plant))
+    plant: function plant(plant) {
+        sessionStorage.setItem('plant_date', JSON.stringify(plant))
+    },
+    currency_rate: function currency_rate(currency_rate) {
+        sessionStorage.setItem('currency_rate', JSON.stringify(currency_rate))
     }
 }
