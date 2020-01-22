2baab75d1d50ac55d6ff6ea02c0efe9613756ef5
Leo <lzweijinwei@yahoo.ca>
2020-01-22 16:45:45 -0500

    update on the first page

---
+++ app/js/model_schemes.js
@@ -0,0 +1,11 @@
+class Project {
+  constructor(name, date, location) {
+    this.name = name;
+    this.date = date;
+    this.location = location;
+  }
+}
+
+module.exports = {
+  Project: Project
+};
