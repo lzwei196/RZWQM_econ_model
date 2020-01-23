665e45af6ee1ca621059e0369c04fe09e0037130
lzwei196 <lzwei1996@hotmail.com>
2020-01-23 11:06:36 -0500

    update on select window

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
-const electron = require("electron")
-const BrowserWindow = electron.remote.BrowserWindow
-const path = require('path')
+const electron = require("electron");
+const BrowserWindow = electron.remote.BrowserWindow;
+const db = require("./js/DB.js").db;
+const path = require("path");
