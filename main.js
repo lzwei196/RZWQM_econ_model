8bfafcfd6855e53bab94f4cb1aa3301e7b81b038
Leo <lzweijinwei@yahoo.ca>
2020-01-24 07:33:17 -0500

    update on window value exchange

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Menu
+} = require("electron");
 const path = require("path");
 const ipc = require("electron").ipcMain;
-s;
 
-const template = [
-  {
+const template = [{
     label: "Start",
-    submenu: [{ role: "About" }]
+    submenu: [{
+      role: "About"
+    }]
   },
   {
     label: "Exit",
-    submenu: [{ role: "close" }]
+    submenu: [{
+      role: "close"
+    }]
   }
 ];
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+ipc.on('reply', (event, message) => {
+  mainWindow.webContents.send('messageFromMain', message);
+})
 
 function createWindow() {
   // Create the browser window.
@@ -38,7 +48,7 @@ function createWindow() {
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -55,19 +65,17 @@ function createWindow() {
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") app.quit();
 });
 
-app.on("activate", function() {
+app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow();
 });
-
-ipc.on("reply", (event, message) => console.log(message));
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
