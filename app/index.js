ecd0ff556f1f9abf60001e6528d415ef446a8505
Leo <lzweijinwei@yahoo.ca>
2020-01-27 09:00:13 -0500

    update on create project

--- app/index.js
+++ app/index.js
@@ -2,16 +2,16 @@ const electron = require("electron");
 const BrowserWindow = electron.remote.BrowserWindow;
 const db = require("./js/DB.js").db;
 const path = require("path");
-const ipc = require('electron').ipcRenderer;
+const ipc = require("electron").ipcRenderer;
 
-let loaded_project_name = '';
+let loaded_project_name = "";
 
-ipc.on('messageFromMain', (event, message) => {
-    console.log(message)
-    loaded_project_name = message;
-    if (loaded_project_name != null) {
-        win.webContents.send('main_reply_from_renderer', '200')
-    } else {
-        win.webContents.send('main_reply_from_renderer', '400')
-    }
+ipc.on("messageFromMain", (event, message) => {
+  console.log(message);
+  loaded_project_name = message;
+  if (loaded_project_name != null) {
+    win.webContents.send("main_reply_from_renderer", "200");
+  } else {
+    win.webContents.send("main_reply_from_renderer", "400");
+  }
 });
